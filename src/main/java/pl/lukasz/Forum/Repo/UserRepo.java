545751bbd6b292df2557787e32b3b5cabb42a6e9
lukasz137test <mufasa137@wp.pl>
2018-07-15 17:59:49 +0200

    Dodanie metody do znajdowania użytkowników po adresie email

--- src/main/java/pl/lukasz/Forum/Repo/UserRepo.java
+++ src/main/java/pl/lukasz/Forum/Repo/UserRepo.java
@@ -1,8 +1,10 @@
 package pl.lukasz.Forum.Repo;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 import pl.lukasz.Forum.Model.User;
 
-public interface UserRepo extends CrudRepository<User,Long> {
+public interface UserRepo extends JpaRepository<User,Long> {
 
+    User findByEmail(String email);
 }
